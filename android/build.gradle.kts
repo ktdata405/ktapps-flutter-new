0f3a8264631bf1108a164f6812e2c89212e61a86
ktdata405 <ktdata405@gmail.com>
2026-09-16 02:03:45 +0530

    Fix: Android SDK setup failure - fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,12 +5,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.set(file("${project.projectDir}/../build"))
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(file("${project.projectDir}/../../build/${project.name}"))
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
